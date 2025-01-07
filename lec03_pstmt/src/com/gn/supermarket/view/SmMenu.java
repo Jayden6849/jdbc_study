f7a71cc274c05430c8162e3139aac844a7c78013
Jayden6849 <jayden6849@gmail.com>
2025-01-07 13:24:54 +0900

    [Jayden] 관리자메뉴, 사용자메뉴 작성 (lec03)
    	- 뷰 작성함으로서 큰 틀만 잡아둠

--- lec03_pstmt/src/com/gn/supermarket/view/SmMenu.java
+++ lec03_pstmt/src/com/gn/supermarket/view/SmMenu.java
@@ -12,7 +12,7 @@ public class SmMenu {
 	public void mainMenu() {
 		while(true) {
 			System.out.println("*** 환영합니다. 우리동네 슈퍼마켓입니다 ***");
-			System.out.println("원하시는 메뉴를 선택해주세요 (종료:99)");
+			System.out.println("원하는 메뉴를 선택해주세요 (종료:99)");
 			System.out.println("1. 회원가입");
 			System.out.println("2. 로그인");
 			System.out.print("메뉴 선택 : ");
@@ -73,15 +73,71 @@ public class SmMenu {
 		if(su != null) {
 			if("admin".equals(su.getUserId())) {
 				// 관리자 메뉴로 이동
-				System.out.println("*** 관리자 메뉴 ***");
+				adminMenu();
 			} else {
 				// 사용자 메뉴로 이동
 				System.out.println("'" + su.getUserNickname() + "'님 환영합니다!");
-				System.out.println("*** 회원 메뉴 ***");
+				userMenu();
 			}
 		} else {
 			System.out.println("아이디와 비밃번호를 다시 확인해주세요.");
 			return;
+		}
+	}
+	
+	public void adminMenu() {
+		while(true) {
+			System.out.println("*** 관리자 메뉴 ***");
+			System.out.println("수행할 작업을 선택하세요 (로그아웃: 99)");
+			System.out.println("1. 제품 등록");
+			System.out.println("2. 제품 입고");
+			System.out.println("3. 판매 현황");
+			System.out.print("메뉴 선택 : ");
+			int menu = scan.nextInt();
+			scan.nextLine();
+			
+			switch(menu) {
+			case 99:
+				System.out.println("로그아웃 되었습니다.");
+				return;
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			default:
+				System.out.println("올바른 메뉴를 선택해주세요.");
+				continue;
+			}
+		}
+	}
+	
+	public void userMenu() {
+		while(true) {
+			System.out.println("*** 회원 메뉴 ***");
+			System.out.println("원하는 메뉴를 선택해주세요 (로그아웃 : 99)");
+			System.out.println("1. 제품 구매");
+			System.out.println("2. 개인 정보 수정");
+			System.out.println("3. 탈퇴");
+			System.out.print("메뉴 선택 : ");
+			int menu = scan.nextInt();
+			scan.nextLine();
+			
+			switch(menu) {
+			case 99:
+				System.out.println("로그아웃 되었습니다.");
+				return;
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			default:
+				System.out.println("올바른 메뉴를 선택해주세요.");
+				continue;
+			}
 		}
 	}
 }
